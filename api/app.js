695394f9ed020d718624868e17d8cab4921ceef2
giangrong123 <lebuihoanggiang6996@gmail.com>
2025-12-04 16:08:34 +0700

    Update

--- api/app.js
+++ api/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+console.log("test");
+
 app.listen(8800, () => {
   console.log('Server is running');
   
---
+++ client/src/App.jsx
@@ -0,0 +1,44 @@
+import HomePage from "./routes/homePage/homePage";
+import ListPage from "./routes/listPage/listPage";
+import SinglePage from "./routes/singlePage/singlePage";
+import ProfilePage from "./routes/profilePage/profilePage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Layout from "./routes/layout/layout";
+
+function App() {
+  const router = createBrowserRouter([
+    { 
+      path: "/", 
+      element: <Layout /> ,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />
+        },
+        { 
+          path: "/list",
+          element: <ListPage />
+        },
+        { 
+          path: "/:id",
+          element: <SinglePage />
+        },
+        { 
+          path: "/profile",
+          element: <ProfilePage />
+        }
+      ]
+    },
+  ]);
+
+  return (
+    <RouterProvider router={router}/>
+  );
+}
+
+export default App;
