c65fe22da32bd70258e33be1ebee5722bbbc5ad2
giangrong123 <lebuihoanggiang6996@gmail.com>
2025-12-13 00:52:42 +0700

    Update

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+export const register = (req, res) => {
+  //db 
+  console.log("register endpoind");
+}
+export const login = (req, res) => {
+  //db 
+}
+export const logout = (req, res) => {
+  //db 
+}
